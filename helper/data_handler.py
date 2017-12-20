7845a4c6207d01dc60ff4de8c3ce85b94d1dc0aa
Johannes Handloser <johannes-handloser@gmx.net>
2017-12-20 12:13:27 +0100

    finalized helper classes: All data is stored in data_handler   - data <- only data columns   - label <- recommended_action for data   - data_frame <- all data as a pandas data frame
     import data_handler in your python notebook file to have data for classification

---
+++ helper/data_handler.py
@@ -0,0 +1,49 @@
+import helper.preprocessor as preprocessor
+import firebase.firebase_wrapper as wrapper
+import pandas as pd
+
+
+class DataHandler:
+
+
+    def __init__(self):
+        self.wrapper = wrapper.Wrapper()
+        self.preprocessor = preprocessor.Preprocessor()
+        self.wrapper.auth_and_login()
+        self.read_out_entries(self.wrapper.db.child("song_data_history").get())
+
+        # split data table into data X and class labels y
+        self.data = self.data_frame.ix[:, 0:10].values
+        self.label = self.data_frame.ix[:, 11].values
+
+
+
+    def read_out_entries(self, all_entries):
+        data_list = []
+
+        for entry in all_entries.each():
+            date_string = entry.key()[:14]
+            if int(date_string) >= 20171129224345:
+                data_list.append(entry)
+
+        for data in data_list:
+            timestamps, gsr_resistance, heart_beat_rate, rr_rate, motiontype, \
+            skin_temp, recommended_action = self.wrapper.get_data_from_pyrebase_object(data)
+
+            self.preprocessor.calculate_feature_vector_list(timestamps, gsr_resistance, heart_beat_rate, \
+                                                       rr_rate, motiontype, skin_temp, recommended_action)
+
+            print(data)
+
+        # creates the whole pandas data Frame
+        self.data_frame = pd.DataFrame(self.preprocessor.feature_vector_list)
+        self.data_frame.columns = ["M(GSR_Res)", "Std(GSR_Res)", "M(HBR)", "Std(HBR)", "M(RR)", "Std(RR)", \
+                                      "RMSSD(RR)", "M(Motion)", "Std(Motion)", "M(ST)", "Std(ST)", "RecommendedAct"]
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-from firebase.firebase_wrapper import *
-from helper.preprocessor import *
-from classifier.classifier import *
+from helper.data_handler import *
 
 if __name__ == '__main__':
 
-    wrapper = Wrapper()
-    preprocessor = Preprocessor()
-    wrapper.auth_and_login()
-
-
-    data_list = []
-
-    all_entries = wrapper.db.child("song_data_history").get()
-    for entry in all_entries.each():
-        entry_key = entry.key()
-        if "11127020586" in entry_key:
-            data_list.append(entry)
-
-    for data in data_list:
-        timestamps, gsr_resistance, heart_beat_rate, rr_rate, motiontype, \
-        skin_temp, recommended_action = wrapper.get_data_from_pyrebase_object(data)
-
-        preprocessor.calculate_feature_vector_list(timestamps, gsr_resistance, heart_beat_rate, \
-                                                   rr_rate, motiontype, skin_temp, recommended_action)
-    #
-    # feature_matrix = pd.DataFrame(preprocessor.feature_vector_list)
-    # feature_matrix.columns = ["M(GSR_Res)", "Std(GSR_Res)", "M(HBR)", "Std(HBR)", "M(RR)", "Std(RR)", \
-    #                           "RMSSD(RR)", "M(Motion)", "Std(Motion)", "M(ST)", "Std(ST)", "Classified"]
-
-
-
-    classifier = Classifier(preprocessor.feature_vector_list)
-    classifier.do_PCA()
+    data_handler = DataHandler()
 
 
 
@@ -45,7 +16,13 @@ if __name__ == '__main__':
 
 
 
-    #preprocessor.visualize_feature_vector_list(preprocessor.feature_vector_list)
+
+
+
+
+
+
+
 
 
 
