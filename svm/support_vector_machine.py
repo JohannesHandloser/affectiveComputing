c06d6ea017f9e8509a58c7b169122b97a978c792
stavros <stavroswd@gmail.com>
2018-01-09 10:54:27 +0100

    Modified SVM

--- svm/support_vector_machine.py
+++ svm/support_vector_machine.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from sklearn.svm import SVC # "Support vector classifier" # choose the model
 
-
 class SupportVectorMachine_Handler:
 
-    def performSVM(self,data):
+    def performSVM(self,matrixX,targeValues):
         model = SVC(kernel='linear', C=1E10)
-        model.fit(X, y)
+        model.fit(matrixX,targeValues) # Matrix + Target Vector
         print (model.support_vectors_)
