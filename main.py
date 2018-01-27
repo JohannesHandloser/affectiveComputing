7555297f2c045e10447247a232f20baa0fc40d08
Johannes Handloser <johannes-handloser@gmx.net>
2018-01-27 15:10:29 +0100

    main to put it all together

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from helper.run import *
 from helper.run_creator import *
 
 if __name__ == "__main__":
-    #ph = PipelineHandler()
+    ph = PipelineHandler()
     #list_of_runs = []
     # list_of_runs.append(Run(user="zarok01", test_data_flag="days", type_of_classifier="dt", split_size=200))
     # list_of_runs.append(Run(user="1162656792", test_data_flag="days", type_of_classifier="rf", n_estimator=10, max_depth=20))
@@ -14,9 +14,14 @@ if __name__ == "__main__":
     #pprint.pprint(result)
 
     run_creator = RunCreator()
-    random_run_list = run_creator.create_random_run_list(10)
+    random_run_list = run_creator.create_random_run_list(3)
     for run in random_run_list:
         pprint.pprint(run.__dict__, indent=5)
+
+    result = ph.pipeline_list_of_runs(random_run_list)
+    pprint.pprint(result)
+
+
 
 
 
